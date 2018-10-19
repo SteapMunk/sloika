cc9b8020f0056074488a146eb87d09669e398937
Ryan Wick <rrwick@gmail.com>
2018-10-19 13:13:12 +1100

    Change pip calls for MASSIVE

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -6,10 +6,10 @@ VIRTUALENV_DIR="${DEV_VIRTUALENV_DIR}"
 
 source ${VIRTUALENV_DIR}/bin/activate
 
-pip install pip --upgrade
+python -m pip install pip --upgrade
 
-pip install -r misc/requirements.txt
+python -m pip install -r misc/requirements.txt
 
-pip install -r setup-dev-env.txt
+python -m pip install -r setup-dev-env.txt
 
 python setup.py develop
